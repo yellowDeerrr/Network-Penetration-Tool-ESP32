589bf9dcbf92ff7165861ab98e4d23905d75c313
kali <emailForGitHubBankSystemSQL@gmail.com>
2025-12-03 15:20:12 +0200

    Fixed delay color logic while changing status of led

--- src/RGB/LedRGB.cpp
+++ src/RGB/LedRGB.cpp
@@ -30,6 +30,8 @@ void LedRGB::handle(unsigned long now){
 }
 
 void LedRGB::handleStatus(ColorsRGB status){
+   if(delayColor) delayColor = false;
+
    switch (status) {
       case INIT:
          setColor(INIT);
@@ -93,25 +95,6 @@ void LedRGB::handleStatus(ColorsRGB status){
  }
 
  void LedRGB::blink(int times){
-    // uint8_t firstBrightness = pixel.getBrightness();
-    // for(int i = 0; i < times; i++){
-    //     turnOff();
-    //     delay(150);
-
-    //     if (lastColor != 0) {
-    //         pixel.setPixelColor(0, lastColor);
-    //     } else {
-    //         // fallback color if LED was off
-    //         pixel.setPixelColor(0, pixel.Color(255, 255, 255)); // white
-    //     }
-
-    //     setBrightness(LED_BRIGHTNESS);
-    //     delay(150);
-    // }
-
-    // pixel.setPixelColor(0, lastColor);
-    // setBrightness(firstBrightness);
-
     uint8_t firstBrightness = pixel.getBrightness();
     for (int i = 0; i < times; i++) {
         turnOff();
